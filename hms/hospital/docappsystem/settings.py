c0336573acca55a1ff20879d5c93f4019a124267
YashJetani <yashjetani123@gmail.com>
2024-11-19 13:30:53 +0530

    settings.py

--- hms/hospital/docappsystem/settings.py
+++ hms/hospital/docappsystem/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'hmspythondb',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
         
